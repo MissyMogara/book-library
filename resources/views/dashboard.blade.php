c0c10ec3442ab08815e21374e03a3fb0820465c0
missymogara <monicagara97@gmail.com>
2025-01-21 11:56:27 +0100

    layout dashboard done

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'DashBoard')
+
+@section('content')
+    <h1>Bienvenido a Mi Aplicación</h1>
+    <p>Esta es la página principal.</p>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Mi Aplicación')</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <header class="p-5 bg-cyan-700 flex justify-around items-center gap-x-4 text-white">
+    <nav class="flex gap-x-6">
+        <a href="#">Inicio</a>
+        <a href="#">Acerca</a>
+        <a href="#">Contacto</a>
+    </nav>
+    </header>
+    <main>
+        @yield('content') 
+    </main>
+    <footer class="p-5 bg-gray-800 flex flex-col justify-around items-center gap-x-4 text-white">
+        <div class="text-center">
+            <p>&copy; 2025 Mi Aplicación</p>
+            <p>Todos los derechos reservados</p>
+        </div>
+        <nav class="flex gap-x-6">
+            <a href="#" class="hover:underline">Política de Privacidad</a>
+            <a href="#" class="hover:underline">Términos de Uso</a>
+            <a href="#" class="hover:underline">Contacto</a>
+        </nav>
+    </footer>
+</body>
+</html>
